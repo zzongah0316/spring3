a94de84b61aebbef5970ca9c587a8554b50b4e32
ITWILL <ITWILL@DESKTOP-L644NRR>
2023-08-04 18:28:24 +0900

    23년 8월 4일

--- spring3/src/main/resources/static/js/test/test.js
+++ spring3/src/main/resources/static/js/test/test.js
@@ -6,16 +6,24 @@
 	 const formSelect = document.querySelector('#form-select');
 	 const action = document.querySelector('#action');
 	 const selectedOptionsSet = new Set();
+	 const price = document.querySelector('dd.price');
+	 const priceText = price.textContent;
+	 const numbersOnly = priceText.replace(/\D/g, '');
 
             formSelect.addEventListener('change', (e) => {
                 console.log('opt');
+                console.log(price.innerHTML);
                 const selectedOption = e.target.value;
                 const optionText = `못난이 파프리카/${selectedOption}`;
                 
                 if (!selectedOptionsSet.has(optionText)) {
                     selectedOptionsSet.add(optionText);
                     const div = document.createElement("div");
-                    div.innerHTML = `<p>${optionText}</p>`;
+                    div.innerHTML = `<p>${optionText}</p>
+                    				<button type="button" class="btn" id="button">-</button>
+                    				<input type="number" class="numBox" min="1" max="10" value="1" readonly="readonly" />
+                    				<button type="button" class="btn" id="button">+</button>
+                    				<label>${numbersOnly}원</label>`;
                     action.appendChild(div);
                 }
 	 });
